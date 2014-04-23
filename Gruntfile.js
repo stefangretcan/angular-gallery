86f9d18d65cfc7ca14cc2f2964e2acf63121f0ce
gion <bogdan.gradinariu@gmail.com>
2014-04-23 08:52:00 +0000

    think I fixed this codeship prob by updating imagemin to v 0.6.0

--- Gruntfile.js
+++ Gruntfile.js
@@ -313,7 +313,7 @@ module.exports = function (grunt) {
         'compass:dist',
         'copy:styles',
         'imagemin',
-        //'svgmin',
+        'svgmin',
         'htmlmin'
       ]
     },
